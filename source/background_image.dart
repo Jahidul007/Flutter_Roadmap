3f18195dafd5fced5a93586997885ffe99b9ada0
Jahidul Islam <jahidul479@gmail.com>
2021-12-17 00:26:55 +0600

    Create background_image.dart

---
+++ source/background_image.dart
@@ -0,0 +1,12 @@
+Container(
+      height: 120.0,
+      width: 120.0,
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage(
+              'assets/assets/alucard.jpg'),
+          fit: BoxFit.fill,
+        ),
+        shape: BoxShape.circle,
+      ),
+    )
